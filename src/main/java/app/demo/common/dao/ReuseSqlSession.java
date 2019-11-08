144fe9b6bf0f33312de2578dde9081d1667e4292
Juho Jeong <aspectran@gmail.com>
2019-11-08 14:54:18 +0900

    Updated

--- src/main/java/app/demo/common/dao/ReuseSqlSession.java
+++ src/main/java/app/demo/common/dao/ReuseSqlSession.java
@@ -1,13 +1,11 @@
 package app.demo.common.dao;
 
-import com.aspectran.core.component.bean.annotation.AvoidAdvice;
 import com.aspectran.core.component.bean.annotation.Bean;
 import com.aspectran.core.component.bean.annotation.Component;
 import com.aspectran.mybatis.SqlSessionAgent;
 
 @Component
 @Bean("reuseSqlSession")
-@AvoidAdvice
 public class ReuseSqlSession extends SqlSessionAgent {
 
     public ReuseSqlSession() {
--- src/main/java/app/demo/common/dao/SimpleSqlSession.java
+++ src/main/java/app/demo/common/dao/SimpleSqlSession.java
@@ -1,13 +1,11 @@
 package app.demo.common.dao;
 
-import com.aspectran.core.component.bean.annotation.AvoidAdvice;
 import com.aspectran.core.component.bean.annotation.Bean;
 import com.aspectran.core.component.bean.annotation.Component;
 import com.aspectran.mybatis.SqlSessionAgent;
 
 @Component
 @Bean("simpleSqlSession")
-@AvoidAdvice
 public class SimpleSqlSession extends SqlSessionAgent {
 
     public SimpleSqlSession() {
