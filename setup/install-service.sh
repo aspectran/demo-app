45c50be4e6115fd12a41e05dfd191574d13cd800
Juho Jeong <aspectran@gmail.com>
2020-01-21 23:06:53 +0900

    Updated

--- setup/install-service.sh
+++ setup/install-service.sh
@@ -7,7 +7,7 @@ echo "Installing /etc/init.d/$APP_NAME ..."
 if [ ! -f "/etc/init.d/$APP_NAME" ]; then
   sudo cp "$REPO_DIR/setup/init.d/service-script" "/etc/init.d/$APP_NAME" || exit
   sudo chmod +x "/etc/init.d/$APP_NAME" || exit
-  sudo update-rc.d "/etc/init.d/$APP_NAME" defaults || exit
+  sudo update-rc.d "$APP_NAME" defaults || exit
   echo "Service $APP_NAME has been installed successfully."
 else
   echo "Service $APP_NAME is already installed."
