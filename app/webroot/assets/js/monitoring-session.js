c93fa8fd74c1fed0a840d0699ddf736733688e10
Juho Jeong <aspectran@gmail.com>
2023-11-30 17:35:08 +0900

    Update

--- app/webroot/assets/js/monitoring-session.js
+++ app/webroot/assets/js/monitoring-session.js
@@ -58,10 +58,12 @@ const SessionStats = function(endpoint, refreshInterval) {
 
     const printStats = function(stats) {
         $(".activeSessionCount").text(stats.activeSessionCount);
-        $(".highestSessionCount").text(stats.highestSessionCount);
+        $(".highestActiveSessionCount").text(stats.highestActiveSessionCount);
+        $(".evictedSessionCount").text(stats.evictedSessionCount);
         $(".createdSessionCount").text(stats.createdSessionCount);
         $(".expiredSessionCount").text(stats.expiredSessionCount);
         $(".rejectedSessionCount").text(stats.rejectedSessionCount);
+        $(".startTime").text(stats.startTime);
         if (stats.currentSessions) {
             $(".sessions").empty();
             stats.currentSessions.forEach(function(username) {
