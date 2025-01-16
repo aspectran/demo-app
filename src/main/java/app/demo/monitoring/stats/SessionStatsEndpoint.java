cc0cf130f8c7ae11d6a11f34a61ebb9af0bf7ca1
Juho Jeong <aspectran@gmail.com>
2025-01-16 14:33:10 +0900

    Update

--- src/main/java/app/demo/monitoring/stats/SessionStatsEndpoint.java
+++ src/main/java/app/demo/monitoring/stats/SessionStatsEndpoint.java
@@ -19,7 +19,7 @@ import com.aspectran.core.activity.InstantActivitySupport;
 import com.aspectran.core.component.bean.annotation.AvoidAdvice;
 import com.aspectran.core.component.bean.annotation.Component;
 import com.aspectran.core.component.session.ManagedSession;
-import com.aspectran.core.component.session.SessionHandler;
+import com.aspectran.core.component.session.SessionManager;
 import com.aspectran.core.component.session.SessionStatistics;
 import com.aspectran.undertow.server.TowServer;
 import com.aspectran.utils.annotation.jsr305.NonNull;
@@ -162,8 +162,8 @@ public class SessionStatsEndpoint extends InstantActivitySupport {
     @NonNull
     private SessionStatsPayload getSessionStatsPayload() {
         TowServer towServer = getBeanRegistry().getBean("tow.server");
-        SessionHandler sessionHandler = towServer.getSessionHandler("root.war");
-        SessionStatistics statistics = sessionHandler.getStatistics();
+        SessionManager sessionManager = towServer.getSessionManager("root.war");
+        SessionStatistics statistics = sessionManager.getStatistics();
 
         SessionStatsPayload stats = new SessionStatsPayload();
         stats.setCreatedSessionCount(statistics.getNumberOfCreated());
@@ -176,9 +176,9 @@ public class SessionStatsEndpoint extends InstantActivitySupport {
 
         // Current Users
         List<String> currentSessions = new ArrayList<>();
-        Set<String> sessionIds = sessionHandler.getActiveSessions();
+        Set<String> sessionIds = sessionManager.getActiveSessions();
         for (String sessionId : sessionIds) {
-            ManagedSession session = sessionHandler.getSession(sessionId);
+            ManagedSession session = sessionManager.getSession(sessionId);
             if (session != null) {
                 currentSessions.add("1:Session " + session.getId() + " created at " +
                         formatTime(session.getCreationTime()));
