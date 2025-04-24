3ebaa98c259e94aa2314363091580387d6b25ee0
Juho Jeong <aspectran@gmail.com>
2025-04-24 22:05:29 +0900

    Update

--- src/main/java/app/demo/common/db/BatchSqlSession.java
+++ src/main/java/app/demo/common/db/BatchSqlSession.java
@@ -3,13 +3,15 @@ package app.demo.common.db;
 import com.aspectran.core.component.bean.annotation.Bean;
 import com.aspectran.core.component.bean.annotation.Component;
 import com.aspectran.mybatis.SqlSessionAgent;
+import org.apache.ibatis.session.ExecutorType;
 
 @Component
-@Bean(id = "batchSqlSession", lazyDestroy = true)
+@Bean(id = "batchSqlSession", lazyDestroy = true, proxied = true)
 public class BatchSqlSession extends SqlSessionAgent {
 
     public BatchSqlSession() {
         super("batchTxAspect");
+        setExecutorType(ExecutorType.BATCH);
         setAutoParameters(true);
     }
 
--- src/main/java/app/demo/common/db/ReuseSqlSession.java
+++ src/main/java/app/demo/common/db/ReuseSqlSession.java
@@ -3,13 +3,15 @@ package app.demo.common.db;
 import com.aspectran.core.component.bean.annotation.Bean;
 import com.aspectran.core.component.bean.annotation.Component;
 import com.aspectran.mybatis.SqlSessionAgent;
+import org.apache.ibatis.session.ExecutorType;
 
 @Component
-@Bean(id = "reuseSqlSession", lazyDestroy = true)
+@Bean(id = "reuseSqlSession", lazyDestroy = true, proxied = true)
 public class ReuseSqlSession extends SqlSessionAgent {
 
     public ReuseSqlSession() {
         super("reuseTxAspect");
+        setExecutorType(ExecutorType.REUSE);
         setAutoParameters(true);
     }
 
